eb55c5f40f9184dd586963c6b475f6b103ee1d62
Ryan Dezall <ryandezall@The-Black-Pearl.lan>
2025-11-22 14:40:43 -0700

    just starting

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import functions as fn
 fn.show_menu()
+
+option = fn.show_menu()
+
+if option == 1:
+    fn.run_search()
+    fn.search()
+else:
+    break
+
+if option == 2:
+    fn.edit_name()
+else:
+    break
